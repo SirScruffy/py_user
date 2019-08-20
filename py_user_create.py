87a9681808455b5c41a687cee0b8ed070aeddf94
SirScruffy <csperl7@gmail.com>
2019-08-20 11:49:55 +0200

    Version 1

---
+++ py_user_create.py
@@ -0,0 +1,19 @@
+import os
+#import mysql.connector
+
+def eingabe():
+
+    user = str(input('Geben Sie bitte den Accountnamen ein.\n'))
+    password = str(input('Geben Sie bitte das Passwort  ein.\n'))
+    #group = str(input('Geben Sie bitte die Gruppe an, zu der der User gehoert.\n'))
+
+    add_user(user,password)
+    return(user,password)
+
+
+def add_user(user,password):
+    os.system('useradd -d /home/'+user+' -m '+ user +' -p $(openssl passwd -1 '+ password +')')
+
+
+
+(user,password) = eingabe()
