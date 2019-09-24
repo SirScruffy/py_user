fee688aa2102f6faf01e8aa3d01f0ffa4c30e740
SirScruffy <csperl7@gmail.com>
2019-09-25 00:28:37 +0200

    Version 1.1

--- py_user_create.py
+++ py_user_create.py
@@ -1,19 +1,58 @@
 import os
-#import mysql.connector
+from mysql.connector import Error
+import MySQLdb
+
 
 def eingabe():
+    #clue is in the name.
 
-    user = str(input('Geben Sie bitte den Accountnamen ein.\n'))
-    password = str(input('Geben Sie bitte das Passwort  ein.\n'))
-    #group = str(input('Geben Sie bitte die Gruppe an, zu der der User gehoert.\n'))
+    host = str(input('Geben Sie bitte den Host ein z.B. localhost.\n'))
+    db_user = str(input('Geben Sie bitte den Accountnamen ein.\n'))
+    database = str(input('Geben Sie bitte die Benutzerdatenbank ein.\n'))
+    # password = str(input('Geben Sie bitte das Passwort  ein.\n'))
 
+    db_connect(host, db_user, database)
+
+
+def add_user(user, password):
+    #Anlegen des Benutzers
+    os.system('useradd -d /home/' + user + ' -m ' + user + ' -p $(openssl passwd -1 ' + password + ')')
+
+
+def db_connect(host, db_user, database):
+    #Aufbau der Verbindung zur Datenbank sowie Ueberpruefung der Eingabe
+
+    try:
+        connection = MySQLdb.connect(host=host,user=db_user,)
+
+
+        if connection.is_connected():
+            print("Verbindung erfolgreich.")
+            cursor = connection.cursor()
+            cursor.execute("use",database,";")
+            userlist = cursor.execute('select * from user_tb')
+            cursor.close()
+            connection.close()
+
+            #print(userlist)
+
+    except Error as error:
+        print("Fehler bei der der Verbindung zur MySQL Datenbank: ", error)
+        print("Geben Sie die Daten neu ein.")
+        eingabe()
+
+    cleaner(userlist)
+
+
+def cleaner(userlist):
+    #Hier wird der Username und das Passwort aus userliste ausgelesen
+    #username an zweiter Stelle und password an dritter
+    #dies klappt nur, da meine Test Datenbank so aufgebaut ist.
+    user = userlist[1]
+    password = userlist[2]
+
+    #zum Schluss werden die Daten an die add_user Funktion Uebergeben
     add_user(user,password)
-    return(user,password)
 
 
-def add_user(user,password):
-    os.system('useradd -d /home/'+user+' -m '+ user +' -p $(openssl passwd -1 '+ password +')')
-
-
-
-(password, user) = eingabe()
+(host, db_user, database) = eingabe()
